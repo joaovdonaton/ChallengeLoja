5fbd53f504262e75042638f244807bccddcab646
joaovdonaton <joao.vitor.donaton@gmail.com>
2022-09-11 15:57:41 -0300

    pagina sobre atualizada

--- src/Loja.java
+++ src/Loja.java
@@ -63,7 +63,7 @@ public class Loja {
             int opcao = promptOpcao();
             if (opcao == 1) compras();
             else if (opcao == 2) autenticarUsuario();
-            else if (opcao == 3) System.out.println("Loja virtual v1.0");
+            else if (opcao == 3) System.out.println("Mercadinho Virtual v1.0 - Desenvolvido por João Vitor Macambira Donaton");
             else if (opcao == 4) { // relatório
                 if(!usuario.isAdmin()){
                     System.out.println("\n [!] Você não é um administrador! ");
